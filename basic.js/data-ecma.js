9a8e5b529aa85bc424c00bb7413509c808980a01
Miansohaib <miansohaib530@gmail.com>
2026-07-06 06:36:35 +0000

    data-ecma

---
+++ basic.js/data-ecma.js
@@ -0,0 +1,20 @@
+"use strick";
+
+
+console.log("Sohaib");
+let age = 20;
+
+console.log(2+2);
+
+//number
+//bigint
+//boolean => true/false
+//string=>""
+//null
+//undefined
+//symble =>unique
+
+
+console.log(typeof null);
+
+console.log(typeof undefined);
