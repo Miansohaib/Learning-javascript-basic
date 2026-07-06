985e667721ea56ea2e28382bdd9dfb48763eafce
Miansohaib <miansohaib530@gmail.com>
2026-07-06 07:14:01 +0000

    conversion of data type

---
+++ basic.js/data-type-conversion.js
@@ -0,0 +1,32 @@
+let score = "29";
+
+console.log(typeof score);
+
+//string to number conversion
+
+let numconvert = Number(score);
+console.log(typeof numconvert);
+
+//string or number conversion to boolean
+// 1 = true : 0 = false
+// name => true
+let loggedin = "sohaib";
+
+console.log(typeof loggedin);
+
+let conloggedin = Boolean(loggedin)
+console.log(typeof conloggedin);
+console.log(conloggedin);
+
+
+//number to string conversion
+
+
+let num = 23;
+
+let connum = String(num);
+console.log(typeof connum);
+
+
+
+
