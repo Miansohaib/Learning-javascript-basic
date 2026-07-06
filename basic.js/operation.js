3c0c03a2e3f389ddb1f7874af1029c4c5903d5e2
Miansohaib <miansohaib530@gmail.com>
2026-07-06 07:52:07 +0000

    operation data

---
+++ basic.js/operation.js
@@ -0,0 +1,21 @@
+// operation 
+
+console.log("1" + 2 +2);
+console.log(1 + 2 +"2");
+
+
+
+console.log(1 + 2 *2 % 3);
+
+console.log(+true);
+console.log(+"");
+
+let gamecounter = 199 ;
+++gamecounter;
+
+console.log(gamecounter);
+
+
+
+
+
