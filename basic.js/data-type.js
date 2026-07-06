ada688bc1a8a5a4264fb508e7372d427c04f8149
Miansohaib <miansohaib530@gmail.com>
2026-07-06 06:38:17 +0000

    data-type config

---
+++ basic.js/data-type.js
@@ -0,0 +1,12 @@
+let First_name = "Mian"
+let second_name = "Sohaib"
+const pi = 3.14
+let age = 22
+let youremail = "abcd@gmail.com"
+let yourcity = "Peshawar"
+
+console.log("My full name is : "+First_name+" "+second_name+","+"My age is "+" "+age+","+"My email is "+" "+youremail+","+"I am from "+" "+yourcity );
+
+console.table([First_name,second_name,pi,age,youremail,yourcity]);
+
+console.log(typeof youremail)
