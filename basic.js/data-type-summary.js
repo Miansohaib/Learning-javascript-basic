3e529887f5101cac6c7c061617ac9d642d22beea
Miansohaib <miansohaib530@gmail.com>
2026-07-07 06:56:57 +0000

    data type summary and changes

---
+++ basic.js/data-type-summary.js
@@ -0,0 +1,30 @@
+// Premitive data type
+// type : String Number Const Null Undefine Boolean Symbol
+let Name = "Mian Sohaib"
+let age = 22;
+const pi = 3.14;
+let myemail = null;
+let mynumber = undefined;
+let addree = Symbol("age");
+let idloggedin = true;
+
+console.log(typeof Name);
+console.log(typeof age);
+console.log(typeof pi);
+console.log(typeof myemail);
+console.log(typeof mynumber);
+console.log(typeof addree);
+console.log(typeof idloggedin);
+
+
+
+
+
+//Non Premitive Data type
+// Array Function Objects
+
+let list = ["Sohaib",22,"asb@gmail.com",2.1];
+console.log(list);
+
+
+
