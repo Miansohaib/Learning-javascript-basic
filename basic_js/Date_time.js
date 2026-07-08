2a2d8f3f6cbb1be8588d3d2d42b4bf2cf420736d
Miansohaib <miansohaib530@gmail.com>
2026-07-08 07:51:37 +0000

    Date and time learn

---
+++ basic_js/Date_time.js
@@ -0,0 +1,18 @@
+const todaydate = new Date;
+console.log(todaydate);
+console.log(todaydate.toString());
+
+console.log(todaydate.toLocaleDateString());
+console.log(todaydate.getDate());
+console.log(Math.round(todaydate.getTime()/1000));
+
+const newnumber = new Date(2026 , 1 , 26);
+
+console.log(newnumber.toString());
+console.log(newnumber.toLocaleString());
+
+
+
+
+
+
