6612e62e5568438410d094e5ac352aef046d06f6
Miansohaib <miansohaib530@gmail.com>
2026-07-08 05:37:54 +0000

    string detail learning

---
+++ basic.js/string.js
@@ -0,0 +1,39 @@
+let myname = "Sohaib khan "
+let num = 20;
+
+console.log(`My name is ${myname} and my age is ${num}`); 
+
+let newstring = myname;
+newstring = "Mian Sohaib";
+console.log(newstring);
+
+let sec = "abcde"
+
+console.log(sec);
+console.log(sec.charAt(2));
+console.log(sec.indexOf('c'));
+console.log(sec.length);
+
+let url = "https//miansohaib20%/khan.com"
+
+console.log(url);
+console.log(url.replace("20%" , "-" ));
+
+console.log(newstring.replace(" ","-"));
+
+let khan = ' Mian Khan '
+console.log(khan);
+console.log(khan.trim());
+console.log(khan.toUpperCase());
+console.log(khan.slice(0, 7));
+
+console.log(khan.repeat(3));
+
+console.log(khan.small(1));
+
+
+
+
+
+
+
