6c6e300c426342d91dd681172952e1a454a7a103
Miansohaib <miansohaib530@gmail.com>
2026-07-13 09:32:15 +0000

    JSON APi

---
+++ arrays_js/object_3.js
@@ -0,0 +1,24 @@
+
+//object de-structure
+
+const course = {
+    coursename : "js in Hindi",
+    price : 999 ,
+    insctroctorname : "Hitash"
+}
+
+const {insctroctorname} = course;
+console.log(insctroctorname);
+
+const {insctroctorname : insctroctor} = course;
+console.log(insctroctor);
+
+
+// JSON
+// {
+
+// }
+//API 
+//{
+//}
+
