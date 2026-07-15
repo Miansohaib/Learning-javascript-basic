4c8b45e93c2e8e1c69f2340b9a20edee022c5acb
Miansohaib <miansohaib530@gmail.com>
2026-07-15 04:57:11 +0000

    local and global scope

---
+++ function_js/Global_and_Local_Scope.js
@@ -0,0 +1,17 @@
+// outside the function or if variables are global scope
+// inside the function or if variables are local scope
+
+const val2 = 400
+
+if (true) {
+   let val1 = 100
+   const val2 = 200
+   var val3 = 3000
+   console.log(val1);
+   
+    
+}
+
+// console.log(val1);
+console.log(val2);
+console.log(val3);
