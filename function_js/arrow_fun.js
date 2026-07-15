034264a81c5e10eb041e8127c2135ee8bd94ce3e
Miansohaib <miansohaib530@gmail.com>
2026-07-15 06:36:05 +0000

    arrow function

---
+++ function_js/arrow_fun.js
@@ -0,0 +1,36 @@
+
+const name = {
+     username   :"Sohaib",
+       name2: function() {
+        console.log(`${this.username}  welcome to website`);
+        console.log(this);
+        
+        
+     }
+}
+
+// name.name2();
+// name.username = "Sam"
+// name.name2();
+
+// console.log(this);
+
+
+// const name1 = function(){
+//    let username1 = "Sam"
+//     console.log(this.username1);
+    
+// }
+// name1()
+
+// const add = (num1,num2) => {
+    
+//     return num1+num2;
+// }
+
+// const add = (num1,num2) =>  (num1+num2);
+
+const add = (num1,num2) =>  (username = "khan");
+
+console.log(add());
+
