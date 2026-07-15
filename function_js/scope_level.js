dcb462f8e682653d59ed0aa75ffa2b5e266e5e7e
Miansohaib <miansohaib530@gmail.com>
2026-07-15 06:02:04 +0000

    scope level

---
+++ function_js/scope_level.js
@@ -0,0 +1,36 @@
+
+function one() {
+    // let username = "sohaib"
+    function two() {
+        let website = "Git hub"
+        console.log(username);
+    }
+    // console.log(website);
+    two()
+}
+// one()
+
+if (true) {
+    let username = "sohaib"
+
+    if (username === "sohaib") {
+        let email = " abcd@gmail.com"
+
+        console.log(username+" " + email);
+        
+    }
+    
+}
+
+function addone(num) {
+    return num + 1
+    
+}
+console.log(addone(4));
+
+
+const addtwo = function(num) {
+    return num + 2
+    
+}
+console.log(addtwo(6));
