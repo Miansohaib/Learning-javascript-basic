ef0b3a5ff7813a629a0e365bcdc0257df53af079
Miansohaib <miansohaib530@gmail.com>
2026-07-15 06:53:38 +0000

    IIFE function

---
+++ function_js/IIFE_fun.js
@@ -0,0 +1,13 @@
+
+(function name() {
+    let username = "Database connected"
+    console.log(username);
+    
+    
+})(); // use ; must
+
+ (  (name4) => {
+    console.log(`database two ${name4}`);
+    
+    
+})('employee')
