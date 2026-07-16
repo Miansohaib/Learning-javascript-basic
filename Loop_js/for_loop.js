06e5bd5e53e17526f4fb57a334ea44665ff64f4a
Miansohaib <miansohaib530@gmail.com>
2026-07-16 09:11:16 +0000

    for loop

---
+++ Loop_js/for_loop.js
@@ -0,0 +1,50 @@
+
+// for (let i = 1; i <= 10; i++) {
+    
+//     console.log(`outer loop value : ${i}`);
+//     for (let j = 1; j <= 10; j++) {
+        
+//         console.log(`outer loop ${i} and inner loop ${j}`);
+        
+        
+//     }
+    
+    
+// }
+
+// for (let i = 1; i <= 10; i++) {
+
+//     console.log(`Table of : ${i}`);
+//     for (let j = 1; j <= 10; j++) {
+        
+//         console.log(`${i} * ${j} = ${i*j}`);
+        
+        
+//     }
+// }
+
+
+
+// let hero = ["Flash" , "Superman","Spiderman",1,2,3]
+// console.log(hero.length);
+
+
+// for (let ndex = 0; ndex < hero.length; ndex++) {
+//     const element = hero[ndex];
+//     console.log(element);
+    
+    
+// }
+
+// continue and break
+
+for (let i = 0; i < 15; i++) {
+    
+    if (i == 7) {
+        console.log("7 is best number");
+        continue
+    }
+    console.log(i);
+    
+    
+}
