a7aff2905d4607ee5dcdbbb3f27a6d7fe3fd0bdd
Miansohaib <miansohaib530@gmail.com>
2026-07-16 10:07:32 +0000

    while and do while loop

---
+++ Loop_js/while_do.js
@@ -0,0 +1,15 @@
+
+// let num = 1
+// while (num < 10) {
+//     console.log(num);
+//      num = num  +  1
+    
+// }
+
+let score = 11
+
+do {
+    console.log(score);
+    score++
+    
+} while (score < 10);
